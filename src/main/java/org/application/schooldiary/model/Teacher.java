e9bc6591b4d1392442e0ba9932b2712fc8707d51
vaibhav345 <vaibhav.mahant@gmail.com>
2017-11-06 13:36:40 -0600

    Added Teacher entity

---
+++ src/main/java/org/application/schooldiary/model/Teacher.java
@@ -0,0 +1,107 @@
+package org.application.schooldiary.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Teacher {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String user_name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String email;
+
+    public Teacher() {}
+
+    public Teacher(String name, String user_name, String password, String email) {
+        this.name = name;
+        this.user_name = user_name;
+        this.password = password;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUser_name() {
+        return user_name;
+    }
+
+    public void setUser_name(String user_name) {
+        this.user_name = user_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", user_name='" + user_name + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Teacher teacher = (Teacher) o;
+
+        if (id != teacher.id) return false;
+        if (name != null ? !name.equals(teacher.name) : teacher.name != null) return false;
+        if (user_name != null ? !user_name.equals(teacher.user_name) : teacher.user_name != null) return false;
+        if (password != null ? !password.equals(teacher.password) : teacher.password != null) return false;
+        return email != null ? email.equals(teacher.email) : teacher.email == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (name != null ? name.hashCode() : 0);
+        result = 31 * result + (user_name != null ? user_name.hashCode() : 0);
+        result = 31 * result + (password != null ? password.hashCode() : 0);
+        result = 31 * result + (email != null ? email.hashCode() : 0);
+        return result;
+    }
+}
